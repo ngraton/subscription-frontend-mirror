e9db41c6db2bfc7344bf86afbec31ca33e8dc29e
Bobbyj23 <jonesrobert1993@gmail.com>
2019-08-05 11:30:44 -0500

    LoginPage added

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Form, Container } from 'react-bootstrap';
+
+
+
+class LoginPage extends Component {
+  state = {
+    username: ''
+  }
+  
+  onChange = async (e) => {
+    await this.setState({username:e.target.value})
+  }
+
+  handleSubmit = () => {
+   //Call API with post request with username as data 
+  }
+  render () {
+    return (
+      <Container>
+        <div align='center'>
+        <Form>
+          <Form.Group controlId="formUserName">
+            <Form.Label>userName</Form.Label>
+            <Form.Control type="text" placeholder="username" onChange={this.onChange}/>
+          </Form.Group>
+        </Form>
+        <button onSubmit={this.handleSubmit}>Submit</button>
+        </div>
+      </Container>
+    )
+  }
+}
+export default LoginPage;
