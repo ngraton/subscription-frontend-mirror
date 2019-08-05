f2060cb5d3a9ce40d50e8379e366894019d14ea1
Bobbyj23 <jonesrobert1993@gmail.com>
2019-08-05 13:34:19 -0500

    SubscriptionForm added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import SubscriptionForm from './forms/SubscriptionForm';
 // import './App.css';
 
 function App() {
   return (
-    <Router></Router>
+    <Router>
+    </Router>
 
   );
 }
