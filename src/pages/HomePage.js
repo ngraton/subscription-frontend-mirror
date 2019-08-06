cf6517e76e7a2f81ac255d67afba634c367b350e
hmburd <h.m.burd@gmail.com>
2019-08-06 13:24:46 -0500

    logout button added to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom'
 import MonthView from '../components/MonthView/MonthView'
 class HomePage extends Component {
+  state = {
+    loggedin: true
+  }
   generateMonthLists() {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     let date = new Date()
@@ -12,10 +15,16 @@ class HomePage extends Component {
       return <MonthView key={index} month={month} />
     })
   }
+  handleLogout(e) {
+    this.setState({loggedin: false})
+  }
+
 
   render() {
     return (
       <div>
+        {this.state.loggedin === false && <Redirect to='/login'/>}
+        <button onClick={(e)=>this.handleLogout(e)}>Logout</button>
         <Link to='/addsubscription'>Add New Subscription</Link>
         {this.generateMonthLists()}
       </div>
