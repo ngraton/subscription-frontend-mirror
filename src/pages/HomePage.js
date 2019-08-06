185e1f63149893e0cc66b61c81df8f23d2130be0
KarinMatsuyama <m.karin2@gmail.com>
2019-08-06 08:59:33 -0500

    added month vew list and single sub show

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import MonthView from '../components/MonthView/MonthView'
 class HomePage extends Component {
+  state = {
+    subscriptions: {}
+  }
+  
+// This is what backend returns
+//   {
+//     "username": "admin",
+//     "subscriptions": [
+//         {
+//             "id": 1,
+//             "name": "big bill",
+//             "due_date": "2019-08-14",
+//             "payment": 100000,
+//             "interval": "annual",
+//             "user": 1
+//         }
+//     ]
+// }
+  componentDidMount() {
+    // API GET request to get all subscriptions for a user
+    // set state subscriptions sorted by month i.g. {'January': [subscriptions for January], 'Febraury': [], ...}
+  }
+
   generateMonthLists() {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     let date = new Date()
@@ -9,7 +32,7 @@ class HomePage extends Component {
     let orderedMonths = months.slice(currentMonth)
     orderedMonths.push(...months.slice(0, currentMonth))
     return orderedMonths.map((month, index) => {
-      return <MonthView key={index} month={month} />
+      return <MonthView key={index} month={month} subscriptions={this.state.subscriptions[month]} />
     })
   }
 
