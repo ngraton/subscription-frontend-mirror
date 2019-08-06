ee36fdc925460b1817fcb6e05d2e3ed93e63354f
ngraton <ngraton@gmail.com>
2019-08-06 09:58:22 -0500

    getUserByUsername API call added

--- src/api/UsersAPI.js
+++ src/api/UsersAPI.js
@@ -1,1 +1,9 @@
-// const url = 'https://subscription-backend.herokuapp.com/api/users'
+import { async } from "q";
+
+const url = 'https://subscription-backend.herokuapp.com/api/users'
+
+const getUserByUsername = async (inputUsername) => {
+  const response = await fetch(`${url}?${inputUsername}`);
+  const data = await response.json();
+  return await data
+}
