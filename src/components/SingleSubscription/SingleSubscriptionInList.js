0f709fe371a75890c5aa5ae9a94701ef38809f65
hmburd <h.m.burd@gmail.com>
2019-08-08 17:30:30 -0500

    made css files for singleSubscription and singlesubscriptioninlist

--- src/components/SingleSubscription/SingleSubscriptionInList.js
+++ src/components/SingleSubscription/SingleSubscriptionInList.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-
-const styles = {
-  subscription_text: {
-    fontWeight: 'normal',
-    fontSize: '20px'
-  }
-}
+import './SingleSubscriptionInList.css'
 
 class SingleSubscription extends Component {
 
   render() {
     return (
-      <Row>
-        <Col><h3 style={styles.subscription_text}>{this.props.subscription.name}</h3></Col>
-        <Col><h3 style={styles.subscription_text}>Due {this.props.subscription.interval}</h3></Col>
-        <Col><h3 style={styles.subscription_text}>$ {this.props.subscription.payment}</h3></Col>
+      <Row className='Subscription-text'>
+        <Col><h3>{this.props.subscription.name}</h3></Col>
+        <Col><h3>Due {this.props.subscription.interval}</h3></Col>
+        <Col><h3>$ {this.props.subscription.payment}</h3></Col>
         {this.props.edit && <Col><Link to={`/editsubscription/${this.props.subscription.id}`}>Edit/Delete</Link> </Col>}
       </Row>
     )
