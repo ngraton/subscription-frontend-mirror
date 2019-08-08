da8d090369286cb9a37c772a4b96097743884128
ngraton <ngraton@gmail.com>
2019-08-08 14:46:24 -0500

    seperated single subscription components

---
+++ src/components/SingleSubscription/SingleSubscriptionInList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+class SingleSubscription extends Component {
+  
+  
+
+  render() {
+    return (
+      <Row>
+        <Col><h3>{this.props.subscription.name}</h3></Col>
+        <Col><h3>Due {this.props.subscription.interval}</h3></Col>
+        <Col><h3>$ {this.props.subscription.payment}</h3></Col>
+        {this.props.edit && <Col><Link to={`/editsubscription/${this.props.subscription.id}`}>Edit/Delete</Link> </Col>}
+      </Row>
+    )
+  }
+}
+
+export default SingleSubscription
