d87ad47ecb175409b45d5d7029399ee43fe22bac
ngraton <ngraton@gmail.com>
2019-08-09 10:13:11 -0500

    added dividered between months

--- src/test.js
+++
@@ -1,7 +0,0 @@
-getDueDate(monthCode, date) {
-  let dueDate = new Date(date)
-  dueDate.setMonth(monthCode)
-  dueDate.setHours(30)
-  dueDate.getMonth() !== monthCode && dueDate.setDate(0)
-  return dueDate.getDate()
-}
