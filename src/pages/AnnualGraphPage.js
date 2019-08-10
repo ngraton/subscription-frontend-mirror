d6d06fd1e88f561712134cd3386d4c2b705022fe
hmburd <h.m.burd@gmail.com>
2019-08-10 14:41:11 -0500

    made an annual page, blank nothing else, waiting for graph

---
+++ src/pages/AnnualGraphPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Form, Button, Alert, InputGroup, Container } from 'react-bootstrap';
+import { Redirect } from 'react-router-dom'
+import SubscriptionsAPI from '../api/SubscriptionsAPI';
+import UsersAPI from '../api/UsersAPI'
+
+
+class MonthlyGraph extends Component {
+  render(){
+  return(
+    <p>Annual Graph</p>
+  )
+
+  
+  }
+}
+
+
+
+export default MonthlyGraph;
